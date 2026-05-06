f32d7a89fc2b648a8efa67db5b9215d3d28e5ac9
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-06 14:30:03 +0200

    Export ReadComicsOnline source from feature branch

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import type { CheerioAPI } from 'cheerio'
 import { MangaWorld } from './MangaWorld/MangaWorld'
+import { ReadComicsOnline } from './ReadComicsOnline/ReadComicsOnline'
 
 declare const cheerio: CheerioAPI
 
 export const MangaWorldSource = new MangaWorld(cheerio)
+export const ReadComicsOnlineSource = new ReadComicsOnline(cheerio)
