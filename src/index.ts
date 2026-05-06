9dcae7713f5e84d3295a74fea78678ea104b4dd8
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-06 18:41:25 +0200

    Remove ReadComicsOnline source registration

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import type { CheerioAPI } from 'cheerio'
 import { MangaWorld } from './MangaWorld/MangaWorld'
-import { ReadComicsOnline } from './ReadComicsOnline/ReadComicsOnline'
 
 declare const cheerio: CheerioAPI
 
 export const MangaWorldSource = new MangaWorld(cheerio)
-export const ReadComicsOnlineSource = new ReadComicsOnline(cheerio)
