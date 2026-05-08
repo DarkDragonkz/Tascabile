c282fb859baa22738802a025303210783980946f
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-08 21:22:34 +0200

    Disable BatCave source export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import type { CheerioAPI } from 'cheerio'
-import { BatCave } from './BatCave/BatCave'
 import { MangaWorld } from './MangaWorld/MangaWorld'
 
 declare const cheerio: CheerioAPI
 
-export const BatCaveSource = new BatCave(cheerio)
 export const MangaWorldSource = new MangaWorld(cheerio)
