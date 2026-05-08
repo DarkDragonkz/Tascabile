bfd5a006be70573a519609bf165ccf265dd95397
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-08 21:24:46 +0200

    Add ReadAllComics constants

---
+++ lib/sources/ReadAllComics/constants.ts
@@ -0,0 +1,3 @@
+export const READ_ALL_COMICS_DOMAIN = 'https://readallcomics.com'
+
+export const READ_ALL_COMICS_PLACEHOLDER_IMAGE = `${READ_ALL_COMICS_DOMAIN}/wp-content/uploads/readallcomics-1.jpg`
