db8965d9bc5e2636d0874567a7271d72a097c965
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-08 20:40:36 +0200

    Add BatCave constants

---
+++ lib/sources/BatCave/constants.ts
@@ -0,0 +1,5 @@
+export const BATCAVE_DOMAIN = 'https://batcave.biz'
+
+export const BATCAVE_IMAGE_DOMAIN = 'https://img.batcave.biz'
+
+export const BATCAVE_PLACEHOLDER_IMAGE = `${BATCAVE_DOMAIN}/android-chrome-192x192.png?v=1.0.0`
