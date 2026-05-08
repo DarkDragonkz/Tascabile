72f2e5d7d2feb58af6abc967791562468725af13
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-08 20:43:49 +0200

    Export BatCave source

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import type { CheerioAPI } from 'cheerio'
+import { BatCave } from './BatCave/BatCave'
 import { MangaWorld } from './MangaWorld/MangaWorld'
 
 declare const cheerio: CheerioAPI
 
+export const BatCaveSource = new BatCave(cheerio)
 export const MangaWorldSource = new MangaWorld(cheerio)
