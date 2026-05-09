120751f07103cd9df829ecfd09d66ac833b2dea3
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-09 10:51:25 +0200

    Add MangaWorld Paperback 0.9 config

---
+++ src/MangaWorld/pbconfig.ts
@@ -0,0 +1,9 @@
+const pbconfig = {
+  contentRating: "EVERYONE",
+  domains: ["https://www.mangaworld.ac"],
+  author: "Kiro Zak",
+  siteName: "MangaWorld",
+  version: "0.9.0",
+};
+
+export default pbconfig;
