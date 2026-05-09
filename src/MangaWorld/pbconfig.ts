965896ca8ae16ec99321a680ca028423ea806a1a
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-09 20:30:41 +0200

    Add MangaWorld Paperback config

---
+++ src/MangaWorld/pbconfig.ts
@@ -0,0 +1,30 @@
+import type { ExtensionInfo } from '@paperback/types'
+import { ContentRating, SourceIntents } from '@paperback/types'
+
+export default {
+  name: 'MangaWorld',
+  description: 'MangaWorld source for Paperback 0.9',
+  version: '0.9.0-alpha.1',
+  icon: 'icon.png',
+  language: 'it',
+  contentRating: ContentRating.MATURE,
+  capabilities: [
+    SourceIntents.DISCOVER_SECTION_PROVIDING,
+    SourceIntents.SEARCH_RESULT_PROVIDING,
+    SourceIntents.MANGA_PROVIDING,
+    SourceIntents.CHAPTER_PROVIDING,
+  ],
+  badges: [
+    {
+      label: 'Italian',
+      textColor: '#ffffff',
+      backgroundColor: '#2c3e50'
+    }
+  ],
+  developers: [
+    {
+      name: 'DarkDragonkz',
+      github: 'https://github.com/DarkDragonkz'
+    }
+  ]
+} satisfies ExtensionInfo
