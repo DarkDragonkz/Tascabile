8a03fe123e4eb1f46d2af9f8f38e568b7efedb60
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-09 10:59:02 +0200

    Use Paperback 0.9 ExtensionInfo for ReadAllComics

--- src/ReadAllComics/pbconfig.ts
+++ src/ReadAllComics/pbconfig.ts
@@ -1,9 +1,25 @@
-const pbconfig = {
-  contentRating: "MATURE",
-  domains: ["https://readallcomics.com"],
-  author: "Kiro Zak",
-  siteName: "ReadAllComics",
-  version: "0.9.0",
-};
+import type { ExtensionInfo } from "@paperback/types";
+import { ContentRating, SourceIntents } from "@paperback/types";
 
-export default pbconfig;
+export default {
+  name: "ReadAllComics",
+  description: "Comic source for ReadAllComics.",
+  version: "1.0.0-alpha.1",
+  icon: "icon.png",
+  language: "en",
+  contentRating: ContentRating.MATURE,
+  capabilities: [
+    SourceIntents.CHAPTER_PROVIDING,
+    SourceIntents.DISCOVER_SECTION_PROVIDING,
+    SourceIntents.SEARCH_RESULT_PROVIDING,
+  ],
+  badges: [
+    { label: "Comics" },
+  ],
+  developers: [
+    {
+      name: "DarkDragonkz",
+      github: "https://github.com/DarkDragonkz",
+    },
+  ],
+} satisfies ExtensionInfo;
