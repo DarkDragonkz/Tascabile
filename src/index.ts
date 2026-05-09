fec9e65585e5732d06f9d64b431c4d1174e43c5f
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-09 10:57:28 +0200

    Remove legacy Paperback 0.8 bootstrap

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import type { CheerioAPI } from 'cheerio'
-import { MangaWorld } from './MangaWorld/MangaWorld'
-
-declare const cheerio: CheerioAPI
-
-export const MangaWorldSource = new MangaWorld(cheerio)
