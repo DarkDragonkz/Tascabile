0e2b83624fa42baf0286521827f4f8af16293ca4
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-09 10:52:05 +0200

    Add ReadAllComics Paperback 0.9 config

---
+++ src/ReadAllComics/pbconfig.ts
@@ -0,0 +1,9 @@
+const pbconfig = {
+  contentRating: "MATURE",
+  domains: ["https://readallcomics.com"],
+  author: "Kiro Zak",
+  siteName: "ReadAllComics",
+  version: "0.9.0",
+};
+
+export default pbconfig;
