277255fa850fea043e3a5db60fa370e89156deee
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-09 20:51:51 +0200

    Bump MangaWorld patch version

--- src/MangaWorld/pbconfig.ts
+++ src/MangaWorld/pbconfig.ts
@@ -4,7 +4,7 @@ import { ContentRating, SourceIntents } from '@paperback/types'
 export default {
   name: 'MangaWorld',
   description: 'MangaWorld source for Paperback 0.9',
-  version: '0.9.0-alpha.1',
+  version: '0.9.0-alpha.2',
   icon: 'icon.svg',
   language: 'it',
   contentRating: ContentRating.MATURE,
