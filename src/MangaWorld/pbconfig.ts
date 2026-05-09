b8709e8c191f4c045c150a3d2e92fef37cd254c1
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-09 10:58:34 +0200

    Use Paperback 0.9 ExtensionInfo for MangaWorld

--- src/MangaWorld/pbconfig.ts
+++ src/MangaWorld/pbconfig.ts
@@ -1,9 +1,25 @@
-const pbconfig = {
-  contentRating: "EVERYONE",
-  domains: ["https://www.mangaworld.ac"],
-  author: "Kiro Zak",
-  siteName: "MangaWorld",
-  version: "0.9.0",
-};
+import type { ExtensionInfo } from "@paperback/types";
+import { ContentRating, SourceIntents } from "@paperback/types";
 
-export default pbconfig;
+export default {
+  name: "MangaWorld",
+  description: "Source italiana per MangaWorld.",
+  version: "1.0.0-alpha.1",
+  icon: "icon.png",
+  language: "it",
+  contentRating: ContentRating.EVERYONE,
+  capabilities: [
+    SourceIntents.CHAPTER_PROVIDING,
+    SourceIntents.DISCOVER_SECTION_PROVIDING,
+    SourceIntents.SEARCH_RESULT_PROVIDING,
+  ],
+  badges: [
+    { label: "Italiano" },
+  ],
+  developers: [
+    {
+      name: "DarkDragonkz",
+      github: "https://github.com/DarkDragonkz",
+    },
+  ],
+} satisfies ExtensionInfo;
