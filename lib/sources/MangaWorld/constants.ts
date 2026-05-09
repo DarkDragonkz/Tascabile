6790ccff96550b5b4305211526128a0851781ca0
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-09 20:23:52 +0200

    Reset repository to fixtures-only baseline

--- lib/sources/MangaWorld/constants.ts
+++
@@ -1,3 +0,0 @@
-export const MANGA_WORLD_DOMAIN = 'https://www.mangaworld.mx'
-
-export const MANGA_WORLD_PLACEHOLDER_IMAGE = `${MANGA_WORLD_DOMAIN}/public/assets/images/MangaWorldSquareLogo.png`
--- lib/sources/ReadAllComics/constants.ts
+++
@@ -1,3 +0,0 @@
-export const READ_ALL_COMICS_DOMAIN = 'https://readallcomics.com'
-
-export const READ_ALL_COMICS_PLACEHOLDER_IMAGE = `${READ_ALL_COMICS_DOMAIN}/wp-content/uploads/readallcomics-1.jpg`
