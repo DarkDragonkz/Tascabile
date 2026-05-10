bfc62fb57616f4c0f597eca78caa8a57463dd1b7
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-10 11:49:37 +0200

    fix: remove unsupported Cloudflare capability from BatCave config

--- src/BatCave/pbconfig.ts
+++ src/BatCave/pbconfig.ts
@@ -1,4 +1,4 @@
-import { ContentRating, SourceIntents } from "@paperback/types";
+import { ContentRating } from "@paperback/types";
 
 import { basePbConfig } from "../generic/config";
 
@@ -8,11 +8,5 @@ pbConfig.name = "BatCave";
 pbConfig.description = "Extension that pulls comic content from batcave.biz.";
 pbConfig.language = "en";
 pbConfig.contentRating = ContentRating.EVERYONE;
-pbConfig.capabilities = [
-  SourceIntents.DISCOVER_SECTION_PROVIDING,
-  SourceIntents.SEARCH_RESULT_PROVIDING,
-  SourceIntents.CHAPTER_PROVIDING,
-  SourceIntents.CLOUDFLARE_BYPASS_PROVIDING,
-];
 
 export default pbConfig;
