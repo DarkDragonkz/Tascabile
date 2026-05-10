2c09d33375596104439c1f523d9fc7e083688616
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-10 11:00:02 +0200

    fix: explicitly set MangaWorld content rating

--- src/MangaWorld/pbconfig.ts
+++ src/MangaWorld/pbconfig.ts
@@ -1,8 +1,11 @@
+import { ContentRating } from "@paperback/types";
+
 import { basePbConfig } from "../generic/config";
 
 const pbConfig = basePbConfig;
 
 pbConfig.name = "MangaWorld";
 pbConfig.description = "Extension that pulls content from www.mangaworld.mx.";
+pbConfig.contentRating = ContentRating.EVERYONE;
 
 export default pbConfig;
