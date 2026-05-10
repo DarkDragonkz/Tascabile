eb9e7b18acad218d837d95421febd25f175b8057
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-10 19:56:18 +0200

    chore: bump version for NineManga localized home labels

--- src/generic/config.ts
+++ src/generic/config.ts
@@ -8,7 +8,7 @@ import {
   type SourceDeveloper,
 } from "@paperback/types";
 
-const BASE_VERSION = "1.0.0-alpha.27";
+const BASE_VERSION = "1.0.0-alpha.28";
 
 export const basePbConfig = {
   name: "",
