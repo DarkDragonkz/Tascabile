da527b80a4daadfbb24264d7f830d9c10b1089ab
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-10 13:49:40 +0200

    chore: bump version for NineManga parser fix

--- src/generic/config.ts
+++ src/generic/config.ts
@@ -8,7 +8,7 @@ import {
   type SourceDeveloper,
 } from "@paperback/types";
 
-const BASE_VERSION = "1.0.0-alpha.9";
+const BASE_VERSION = "1.0.0-alpha.10";
 
 export const basePbConfig = {
   name: "",
