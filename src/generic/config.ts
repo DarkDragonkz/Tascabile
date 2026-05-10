144ab61fc773723e3851fc33defe519755551a00
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-10 15:27:51 +0200

    chore: bump version for NineManga chapter image filtering

--- src/generic/config.ts
+++ src/generic/config.ts
@@ -8,7 +8,7 @@ import {
   type SourceDeveloper,
 } from "@paperback/types";
 
-const BASE_VERSION = "1.0.0-alpha.16";
+const BASE_VERSION = "1.0.0-alpha.17";
 
 export const basePbConfig = {
   name: "",
