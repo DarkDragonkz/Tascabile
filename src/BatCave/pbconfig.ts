eeb3055fa9f20363d41ca87a192af69bc85f6c0e
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-10 11:45:33 +0200

    fix: enable BatCave Cloudflare bypass capability

--- src/BatCave/pbconfig.ts
+++ src/BatCave/pbconfig.ts
@@ -1,4 +1,4 @@
-import { ContentRating } from "@paperback/types";
+import { ContentRating, SourceIntents } from "@paperback/types";
 
 import { basePbConfig } from "../generic/config";
 
@@ -8,5 +8,11 @@ pbConfig.name = "BatCave";
 pbConfig.description = "Extension that pulls comic content from batcave.biz.";
 pbConfig.language = "en";
 pbConfig.contentRating = ContentRating.EVERYONE;
+pbConfig.capabilities = [
+  SourceIntents.DISCOVER_SECTION_PROVIDING,
+  SourceIntents.SEARCH_RESULT_PROVIDING,
+  SourceIntents.CHAPTER_PROVIDING,
+  SourceIntents.CLOUDFLARE_BYPASS_PROVIDING,
+];
 
 export default pbConfig;
