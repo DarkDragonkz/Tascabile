1ce6a1b42a67144d7499312806b898aaabf26028
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-10 19:01:48 +0200

    chore: bump version for NineManga status parsing

--- src/generic/config.ts
+++ src/generic/config.ts
@@ -8,7 +8,7 @@ import {
   type SourceDeveloper,
 } from "@paperback/types";
 
-const BASE_VERSION = "1.0.0-alpha.24";
+const BASE_VERSION = "1.0.0-alpha.25";
 
 export const basePbConfig = {
   name: "",
