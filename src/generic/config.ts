d1a1bc51e041b51782d18559d90f2d8694a62f24
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-10 15:04:08 +0200

    chore: bump version for NineManga chapter chunk fix

--- src/generic/config.ts
+++ src/generic/config.ts
@@ -8,7 +8,7 @@ import {
   type SourceDeveloper,
 } from "@paperback/types";
 
-const BASE_VERSION = "1.0.0-alpha.14";
+const BASE_VERSION = "1.0.0-alpha.15";
 
 export const basePbConfig = {
   name: "",
