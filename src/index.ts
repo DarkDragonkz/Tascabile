26cb4543ddae8f8182697a861ea4c29605e5c74c
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-10 09:42:59 +0200

    Remove

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import type { CheerioAPI } from 'cheerio'
-import { MangaWorld } from './MangaWorld/MangaWorld'
-import { ReadAllComics } from './ReadAllComics/ReadAllComics'
-
-declare const cheerio: CheerioAPI
-
-export const MangaWorldSource = new MangaWorld(cheerio)
-export const ReadAllComicsSource = new ReadAllComics(cheerio)
