7b9f6c4b028a864ff9d0fe2a6b89ce515bb48446
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-10 14:31:49 +0200

    chore: bump version for NineManga regex fallback

--- src/generic/config.ts
+++ src/generic/config.ts
@@ -8,7 +8,7 @@ import {
   type SourceDeveloper,
 } from "@paperback/types";
 
-const BASE_VERSION = "1.0.0-alpha.12";
+const BASE_VERSION = "1.0.0-alpha.13";
 
 export const basePbConfig = {
   name: "",
