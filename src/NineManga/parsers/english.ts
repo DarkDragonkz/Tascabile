eb35979bc9aa2d795d849a3a704ccafdf624a557
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-11 20:55:02 +0200

    Enhance englishReaderParser with deduplication and new image parsing
    Updated the englishReaderParser to include deduplication and additional image parsing.

--- src/NineManga/parsers/english.ts
+++ src/NineManga/parsers/english.ts
@@ -1,6 +1,9 @@
 import type { NineMangaReaderParser } from "./common";
-import { parsePicBoxImages } from "./common";
+import { parseAllImgsUrlArray, parsePicBoxImages, dedupeStrings } from "./common";
 
-export const englishReaderParser: NineMangaReaderParser = (_html, $, baseUrl) => {
-  return parsePicBoxImages($, baseUrl);
+export const englishReaderParser: NineMangaReaderParser = (html, $, baseUrl) => {
+  return dedupeStrings([
+    ...parsePicBoxImages($, baseUrl),
+    ...parseAllImgsUrlArray(html, baseUrl),
+  ]);
 };
