0f1fb25ae9ad6af0f5713fd33a9fdb4b9da39dcd
DarkDragonkz <kirolosmarco2013@gmail.com>
2026-05-12 18:13:37 +0200

    Route NineManga languages to dedicated reader parsers

--- src/NineManga/parsers/index.ts
+++ src/NineManga/parsers/index.ts
@@ -1,21 +1,36 @@
 import type { NineMangaReaderParser } from "./common";
 import { englishReaderParser } from "./english";
+import { frenchReaderParser } from "./fr";
+import { germanReaderParser } from "./de";
 import { italianReaderParser } from "./italian";
+import { portugueseBrReaderParser } from "./ptBr";
+import { russianReaderParser } from "./ru";
+import { spanishReaderParser } from "./es";
 
 export type { NineMangaReaderParser } from "./common";
 export { englishReaderParser } from "./english";
+export { frenchReaderParser } from "./fr";
+export { germanReaderParser } from "./de";
 export { italianReaderParser } from "./italian";
+export { portugueseBrReaderParser } from "./ptBr";
+export { russianReaderParser } from "./ru";
+export { spanishReaderParser } from "./es";
 
 export function getNineMangaReaderParser(language: string): NineMangaReaderParser {
   switch (language) {
     case "eng":
       return englishReaderParser;
-    case "ita":
     case "esp":
+      return spanishReaderParser;
     case "rus":
+      return russianReaderParser;
     case "deu":
+      return germanReaderParser;
     case "fra":
+      return frenchReaderParser;
     case "br":
+      return portugueseBrReaderParser;
+    case "ita":
     default:
       return italianReaderParser;
   }
